ebbfbf2b96227fac5371a4a870d2c7779547b8b3
macournoyer <macournoyer@gmail.com>
2008-10-22 00:56:31 -0400

    Add script to run and compile source

--- lib/orange.rb
+++ lib/orange.rb
@@ -6,21 +6,16 @@ require "orange/generator"
 require "orange/nodes"
 
 if File.file?(File.dirname(__FILE__) + "/orange/grammar.rb")
+  # Take compiled one
   require "orange/grammar"
 else
   Treetop.load File.dirname(__FILE__) + "/orange/grammar.tt"
 end
 
-if __FILE__ == $PROGRAM_NAME
-  g = Orange::Generator.new
-  OrangeParser.new.parse(<<-EOS).compile(g).to_s
-    def test()
-      x = "ohaie"
-      printf(x)
-    end
-    test()
-  EOS
-  puts g.inspect
-  # g.optimize
-  g.run
+module Orange
+  def self.compile(code)
+    g = Orange::Generator.new
+    OrangeParser.new.parse(code).compile(g)
+    g
+  end
 end
